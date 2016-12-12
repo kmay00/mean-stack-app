694f467e70c94021c57c76be9305efb3c9e1a7df
kmay00 <kmay00@gmail.com>
2016-12-12 13:03:06 -0500

    fix router bug, add post

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var routes = require('./api/routes');
 
+// Define the port
 app.set('port', 3000);
 
+// Add middleware to console log every request
 app.use(function(req, res, next) {
   console.log(req.method, req.url);
   next();
 });
 
+// Set static directory before joining routes
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Body parser middleware
+app.use(bodyParser.urlencoded( { extended : false } ));
+
+// Add routing
 app.use('/api', routes);
 
+// Listen for requests
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
   console.log('Magic happens on port ' + port);
